f64beb94dd4778286e4e53d9109ce9b7ce86c5cf
anshukas <anshubtechinit@gmail.com>
2017-04-13 16:54:08 +0530

    Trying SOAP Ws with authentication part 2

---
+++ Calc3/src/com/calc3/Calc3Impl.java
@@ -0,0 +1,10 @@
+package com.calc3;
+
+public class Calc3Impl implements ICalc3 {
+
+	@Override
+	public int add(int x, int y) {
+		return x+y;
+	}
+
+}
---
+++ Calc3/src/com/calc3/ICalc3.java
@@ -0,0 +1,6 @@
+package com.calc3;
+
+public interface ICalc3 {
+	
+	public int add(int x,int y);
+}
